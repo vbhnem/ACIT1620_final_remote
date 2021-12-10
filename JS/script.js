a8d150c6a0dc41442bbd5a81c63408cf2d12cefa
vbhnem <90465522+vbhnem@users.noreply.github.com>
2021-12-10 13:30:21 -0800

    Create eventListener to submit

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,34 @@
 const body = document.querySelector(".body")
-const toggle = document.querySelector(".toggle")
+const submit = document.querySelector("#frm-btn")
 
-consple.log(darktheme())
+const Contentinfo = [
+                   { 
+                    email: "youremail@fake.com", 
+                    name: "your name", 
+                    service: "Some service, facial transmogrification", 
+                    message: "Your message about the service, it doesn't tranmogrify my face" 
+                   }
+                 ]
 
-function darktheme() {
-    let checkBox = document.getElementById("checkBox")
-    if (checkBox.checked == True) {
-        document.getElementsById ("checkBox").className = "dark-theme"
+submit.addEventListener("click", (e) =>{
+    const message = document.querySelector(".message")
+    const name = document.querySelector(".name")
+    const email = document.querySelector(".email")
+   
+    let content = {
+        message: message.value,
+        name: name.value,
+        email: email.value
     }
+
+    Contentinfo.push(content)
+    e.preventDefault()
+})
+
+
+let checkBox = document.getElementById("checkBox")
+if (checkBox.checked == True) {
+    document.getElementsById ("checkBox").className = ".dark-theme"
 }
+
+
